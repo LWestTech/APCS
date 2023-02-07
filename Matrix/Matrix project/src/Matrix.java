b07de398bbbc37b30db9786a1c9800ace61af3df
Lucas West <91633686+LWestTech@users.noreply.github.com>
2023-02-07 03:35:52 +0000

    f

--- Matrix/Matrix project/src/Matrix.java
+++ Matrix/Matrix project/src/Matrix.java
@@ -2,20 +2,20 @@ public class Matrix
 {
     private int[][] arr;
 
-    /*
+    /**
      * Constructs a matrix
      * @param rows number of rows
      * @param cols number of columns
      */
     public Matrix(int rows, int cols) {this.arr = new int[rows][cols];}
 
-    /*
+    /**
      * Constructs a matrix from a 2d array
      * @param arr 2d array
      */
     public Matrix(int[][] arr) {this.arr = arr;}
 
-    /*
+    /**
      * The matrix as a string
      * @return string representing this matrix
      */
@@ -36,7 +36,7 @@ public class Matrix
         return str;
     }
 
-    /*
+    /**
      * Checks to see if the contents of this and another matrix are identical
      * @param mtr matrix to compair with
      * @return if the two matrices match
@@ -61,7 +61,7 @@ public class Matrix
         return false;
     }
 
-    /*
+    /**
      * Sets the value of any given cell in the matrix
      * @param row cell's row index
      * @param col cell's column index
@@ -69,33 +69,33 @@ public class Matrix
      */
     public void set(int row, int col, int content) {arr[row][col] = content;}
     
-    /*
+    /**
      * Sets the entire matrix to contain a different set of data
      * @param arr two-dimensional int array
      */
     public void set(int[][] arr) {this.arr = arr;}
 
-    /*
+    /**
      * Gets the content of a specific cell
      * @param row cell's row index
      * @param col cell's column index
      */
     public int get(int row, int col) {return arr[row][col];}
 
-    /*
+    /**
      * Gets the 2d array that holds the data of the matrix
      * @return two-dimensional int array
      */
     public int[][] get() {return arr;}
 
-    /*
+    /**
      * Gets an int array containing all values in a specific row
      * @param row index of the row whose values are wanted
      * @return int array of values
      */
     public int[] getRow(int row) {return arr[row];}
 
-    /*
+    /**
      * Gets an int array containing all values in a specific column
      * @param col index of the column whose values are wanted
      * @return int array of values
@@ -109,19 +109,19 @@ public class Matrix
         return colArr;
     }
     
-    /*
+    /**
      * Gets the total number of rows in the matrix
      * @return number of rows
      */
     public int getRows() {return arr.length;}
 
-    /*
+    /**
      * Gets the total number of columns in the matrix
      * @return number of columns
      */
     public int getCols() {return arr[0].length;}
 
-    /*
+    /**
      * Gets a 2d int array containing values from every cell in the matrix except
      * those in the first row and those that are in the specified column
      * @param col the horizontal index of the column to be excluded
--- Matrix/Matrix project/src/MatrixMath.java
+++ Matrix/Matrix project/src/MatrixMath.java
@@ -1,19 +1,21 @@
-import javax.naming.directory.InvalidAttributesException;
-
 enum Operation
 {
     ADD, SUBTRACT, MULTIPLY
 }
+
+/**
+ * Perform mathmatical operations with <code>Matrix</code> objects
+ */
 public class MatrixMath
 {
-    /*
+    /**
      * Overides the <code>Object</code> class's constructor and makes it private. This prevents this
      * class from ever being instantiated, allowing it to behave like a static class
      */
     private MatrixMath() {} // A handy little trick to prevent initialization of a class
 
     
-    /*
+    /**
      * Calculates the determinant of any matrix passed into the method
      * @param mtr matrix
      * @return determinant
@@ -68,63 +70,118 @@ public class MatrixMath
         return determinant;   
     }
 
-    public static Matrix calculate(Matrix mtrA, Matrix mtrB, Operation operation)
+    /**
+     * Applies a certain matrix manipulation to two matrices to create a new matrix
+     * @param mtrA left matrix
+     * @param mtrB right matrix
+     * @param operation what matrix manipulation operation to apply
+     * @return the resulting matrix
+     */
+    private static Matrix calculate(Matrix mtrA, Matrix mtrB, Operation operation)
     {
-        if (mtrA.getCols() != mtrB.getCols() || mtrA.getRows() != mtrB.getRows())
+        if (operation != Operation.MULTIPLY)
         {
-            throw new IllegalArgumentException("Can\'t add matrices of different sizes");
+            if (mtrA.getCols() != mtrB.getCols() || mtrA.getRows() != mtrB.getRows())
+            {
+                throw new IllegalArgumentException("Can\'t add or subtract matrices of different sizes");
+            }
+        } else if (mtrA.getCols() != mtrB.getRows())
+        {
+            throw new IllegalArgumentException("Can\'t multiply matrices of non-corresponding sizes");
         }
+        
+        int rows = mtrA.getRows();
+        int cols = mtrB.getCols();
 
+        Matrix output = new Matrix(rows, cols);
+
+        for (int r = 0; r < rows; r++)
+        {
+            for (int c = 0; c < cols; c++)
+            {
+                int newData = 0;
+                /*
+                 * I know you hate switch statements, but you've gotta admit, this is a really clean way to do this
+                 * Switches just work so nicely with enums that its hard to resist. If you really hate it, I can
+                 * re-do it.
+                 * Oh, also, having a <code>calculate()</code> method using the enum prevents a lot of copy/pasting
+                 */
+                switch (operation)
+                {
+                    case ADD:
+                        newData = mtrA.get(r, c) + mtrB.get(r, c);
+                        break;
+                    case SUBTRACT:
+                        newData = mtrA.get(r, c) - mtrB.get(r, c);
+                        break;
+                    case MULTIPLY:
+                        for (int i = 0; i < mtrA.getCols(); i++)
+                        {
+                            newData += mtrA.get(r, i) * mtrB.get(i, c);
+                        };
+                        break;
+                }
+
+                output.set(r, c, newData);
+            }
+        }
+        return output;
+    }
+
+    /**
+     * Adds two matrices
+     * @param mtrA matrix a
+     * @param mtrB matrix b
+     * @return the resulting matrix
+     */
+    public static Matrix add(Matrix mtrA, Matrix mtrB)
+    {
+        return calculate(mtrA, mtrB, Operation.ADD);
+    }
+
+    /**
+     * Subtracts two matrices
+     * @param mtrA matrix a
+     * @param mtrB matrix b
+     * @return the resulting matrix
+     */
+    public static Matrix subtract(Matrix mtrA, Matrix mtrB)
+    {
+        return calculate(mtrA, mtrB, Operation.SUBTRACT);
+    }
+
+    /**
+     * Multiplies two matrices
+     * @param mtrA left matrix
+     * @param mtrB right matrix
+     * @return the resulting matrix
+     */
+    public static Matrix multiply(Matrix mtrA, Matrix mtrB)
+    {
+        return calculate(mtrA, mtrB, Operation.MULTIPLY);
+    }
+
+    /**
+     * Multiplies a matrix by a scalar
+     * @param mtrA matrix
+     * @param scalar integer scalar to scale the matrix
+     * @return the resulting matrix
+     */
+    public static Matrix scalarMultiply(Matrix mtrA, int scalar)
+    {
         int rows = mtrA.getRows();
         int cols = mtrA.getCols();
 
         Matrix output = new Matrix(rows, cols);
 
-        for (int r = 1; r < rows; r++)
+        for (int r = 0; r < rows; r++)
         {
             for (int c = 0; c < cols; c++)
             {
-                int cellA = mtrA.get(r, c);
-                int cellB = mtrB.get(r, c);
-
-                int newData = 0;
-                switch (operation)
-                {
-                    case ADD:
-                        newData = cellA + cellB;
-                        break;
-                    case SUBTRACT:
-                        newData = cellA - cellB;
-                        break;
-                    case MULTIPLY:
-                        newData = cellA * cellB;
-                        break;
-                }
-                
-                output.set(r, c, newData);
+                output.set(r, c, mtrA.get(r, c) * scalar);
             }
         }
 
         return output;
-    }
-
-    public static Matrix add(Matrix mtrA, Matrix mtrB)
-    {
-        
-    }
-
-    public static Matrix subtract(Matrix mtrA, Matrix mtrB)
-    {
-
-    }
-
-    public static Matrix multiply(Matrix mtrA, Matrix mtrB)
-    {
-
-    }
-
-    public static Matrix scalarMultiply(Matrix mtrA, int scalar)
-    {
-
     }
 }
