105e0d7c9c2cf6c508f19807a86eca3bf5b20a58
Lucas West <91633686+LWestTech@users.noreply.github.com>
2023-03-20 00:22:38 +0000

    s

--- Merge Sort/src/Sorts.java
+++ Merge Sort/src/Sorts.java
@@ -26,12 +26,54 @@ public class Sorts
     public static boolean isSorted()
     // Determine whether the array values are sorted
     {
+        int prev = 0;
+        for (int current : values)
+        {
+            if (prev > current) return false;
+        }
+
+        return true;
     }
     
     public static void swap(int index1, int index2)
     // Swaps the integers at locations index1 and index2 of array values
     // Precondition:  index1 and index2 are less than size
     {
+        int temp = values[index1];
+        values[index1] = values[index2];
+        values[index2] = temp;
+    }
+
+    public static void merge(int leftFirst, int leftLast, int rightFirst, int rightLast)
+    {
+        if (rightLast - leftFirst <= 1) return;
+        int[] drawList = new int[rightLast - leftFirst + 1];
+        drawList = values.clone();
+        int leftPointer = leftFirst;
+        int rightPointer = rightFirst;
+        for (int i = leftFirst; i <= rightLast; i++)
+        {
+            int a = drawList[leftPointer];
+            int b = drawList[rightPointer];
+            values[i] = (a > b)? b : a;
+            
+        }
+        while (leftPointer < leftLast || rightPointer < rightLast)
+        {
+            
+        }
+    }
+
+    public static int div(int a, int b)
+    {
+        double decimalForm = a / b;
+        return (int)(decimalForm + 0.5);
+    }
+
+    public static void mergeSort()
+    {
+        int split = div(SIZE, 2);
+        merge(0, split, split + 1, SIZE - 1);
     }
     
     public static void printValues()
