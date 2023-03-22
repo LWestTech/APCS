c7bf4f4b2737544349b9c478df74c140eda3f062
ank-ank <128556661+ank-ank@users.noreply.github.com>
2023-03-22 19:28:35 +0000

    he he ha ha

---
+++ S0rtingProject/src/Sorts.java
@@ -0,0 +1,37 @@
+public class QuickSort
+{
+    public static void quickSort(int from, int to)
+    {
+        if (from < to)
+        {
+            int p = split(from, to);
+            quickSort(from, p - 1);
+            quickSort(p + 1, to);
+        }
+    }
+
+    private static int split(int from, int to)
+    {
+        int pivot = values[from];
+        int first = from;
+        int last = to;
+        while (first < last)
+        {
+            first++;
+            while (first < SIZE && values[first] <= pivot)
+            {
+                first++;
+            }
+            while (values[last] > pivot)
+            {
+                last--;
+            }
+            if (first < last)
+            {
+                swap(first, last);
+            }
+        }
+        Swaps.swap(from, last);
+        return last;
+    } 
+}   
