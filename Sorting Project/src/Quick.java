6f07a63523f83eec566a63b2d334adb6db80d68d
ank-ank <128556661+ank-ank@users.noreply.github.com>
2023-03-23 15:37:53 -0400

    quck java sort

--- Sorting Project/src/Quick.java
+++ Sorting Project/src/Quick.java
@@ -1,5 +1,37 @@
-public class Quick {
-    j nl joljjbhblj
-        
-        
+public class Quick extends Sorts
+{
+    public static void quickSort(int from, int to)
+    {
+        if (from < to)
+        {
+            int p = split(from, to);
+            quickSort(from, p - 1);
+            quickSort(p + 1, to);
+        }
+    }
+    private static int split(int from, int to)
+    {
+        int pivot = values[from];
+        int first = from;
+        int last = to;
+        while (first < last)
+        {
+            first++;
+            while (first < SIZE && values[first] <= pivot)
+            {
+                first++;
+            }
+            while (values[last] > pivot)
+            {
+                last--;
+            }
+            if (first < last)
+            {
+                swap(first, last);
+            }
+        }
+        swap(from, last);
+        return last;
+    }
 }
+
