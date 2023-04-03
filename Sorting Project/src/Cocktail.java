279786acb74d9213ea209e4684e5a89f0b3dd8f4
Lucas West <91633686+LWestTech@users.noreply.github.com>
2023-04-03 00:19:36 +0000

    8:19 pm
    Co-authored-by: ank-ank <ank-ank@users.noreply.github.com>
    Co-authored-by: RaspberryLime123 <RaspberryLime123@users.noreply.github.com>

--- Sorting Project/src/Cocktail.java
+++ Sorting Project/src/Cocktail.java
@@ -1,3 +1,25 @@
-public class Cocktail {
-    
+public class Cocktail extends Sorts
+{
+    public Cocktail(int size)
+    {
+        super(size);
+    }
+
+    public void cocktailSort()
+    {
+        int startInd = 0;
+        int endInd = getSize() - 1;
+        boolean swapped = true;       
+
+        while (swapped == true)
+        {
+            swapped = false;
+
+            for (int i = startInd; i < endInd - 1; i++)
+            {
+                if (getVali(i) > getVali
+            }
+        }
+
+    }
 }
